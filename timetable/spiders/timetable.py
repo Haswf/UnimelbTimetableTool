35dc69d2374dc0ab7c6783eeff07ef8004e04b6e
Haswell <haswell.fan@outlook.com>
2019-07-25 19:20:05 +1000

    Add LoginCredential class for spider-wise control of credential json file, Add method validate_login to check if login was succeed

--- timetable/spiders/timetable.py
+++ timetable/spiders/timetable.py
@@ -1,14 +1,21 @@
 import scrapy
 import os
+from LoginCredential import LoginCredential
 from pathlib2 import Path
 from timetable.items import Class
-import json
-from scrapy.http import Request
-from scrapy.utils.response import open_in_browser
+from scrapy.exceptions import CloseSpider
+import logging
 
 class TimeTableSpider(scrapy.Spider):
     name = "timetable"
     start_urls = ['https://prod.ss.unimelb.edu.au/student/Login.aspx']
+    # credential file name that contains username and password
+    credential_file = 'uom.json'
+    credential_obj = None
+
+    def __init__(self):
+        super().__init__()
+        self.credential_obj = LoginCredential(filepath=self.get_credential_path())
 
     def start_requests(self):
         for url in self.start_urls:
@@ -16,66 +23,59 @@ class TimeTableSpider(scrapy.Spider):
 
     def log_in(self, response):
         # TODO: Doc required
-        self.log("Getting tokens for login")
-        username, password = self.get_login_credential()
+        self.log(level=logging.INFO, message="Getting tokens for login")
+        # call credential_obj to get username and password
+        self.credential_obj.get_credential()
+
+        # Construct form data
         # TODO：get __EVENTTARGET from response instead of hard coding it
         formdata = {'__EVENTTARGET': 'ctl00$Content$cmdLogin',
                     '__VIEWSTATE': response.xpath("//div/input[@name='__VIEWSTATE']/@value").get(),
                     '__VIEWSTATEGENERATOR': response.xpath("//*/div/input[@name='__VIEWSTATEGENERATOR']/@value").get(),
                     '__EVENTVALIDATION': response.xpath("//div/input[@name='__EVENTVALIDATION']/@value").get(),
-                    'ctl00$Content$txtUserName$txtText': username,
-                    'ctl00$Content$txtPassword$txtText': password,
+                    'ctl00$Content$txtUserName$txtText': self.credential_obj.username,
+                    'ctl00$Content$txtPassword$txtText': self.credential_obj.password
                     }
 
         # validate tokens
         for token in formdata.values():
             if not len(token):
-                raise ValueError("Invalid login token(s): {}".format(token))
+                # close spider if invalid was provided
+                self.log(level=logging.CRITICAL, message="Invalid login token(s): {}".format(token))
+                raise CloseSpider("Invalid login token(s): {}".format(token))
 
         return scrapy.FormRequest.from_response(response, formdata=formdata, callback=self.open_timetable)
+    #
+    # def parse(self, response):
+    #    return
 
-
-    def get_login_credential(self):
-        # TODO: Doc required
-        # credential file name that contains username and password
-        CREDENTIAL_FILE_NAME = 'uom.json'
-
-        # Get path of credential json file
+    def get_credential_path(self):
+        """
+        Get real path of credential json file
+        :return:
+        """
         __location__ = Path(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))))
-        credential_path = Path(os.path.join(__location__.parent.parent,  # equivalent to .../uom.json
-                                            CREDENTIAL_FILE_NAME))
+        return Path(os.path.join(__location__.parent.parent,  # equivalent to .../uom.json
+                                            self.credential_file))
 
-        # if credential exists in project directory
-        if credential_path.exists():
-            self.log('Reading credential from json')
-            with open(credential_path) as f:
-                credential = json.load(f)
+    def validate_login(self, response):
+        # Validate if log in was successful
+        login_summary = response.xpath("//div[@class='cssT1SmBasicFormValidationSummary']").get()
 
-        # read and save credential from user input
-        else:
-            credential = dict()
-            credential['username'] = input("What's your UoM username: ")
-            credential['password'] = input("Then, what's your password: ")
-            with open(credential_path, 'w') as f:
-                json.dump(credential, f)
-                self.log("Credential saved to {}".format(credential_path))
+        if (login_summary and "Your login attempt was not successful" in login_summary):
+            # In this case, close spider and delete credential file
+            self.log(level=logging.CRITICAL, message='Login failed: Incorrect username or/and password')
+            self.credential_obj.remove_file()
+            raise CloseSpider(reason='Login failed: Incorrect username and/or password')
 
-        # return username and password
-        return credential['username'], credential['password']
-
-    def parse(self, response):
-       return
-        
-    def open_timetable(self,response):
-        # TODO: Doc requried
+    def open_timetable(self, response):
+        # validate if login is successful
+        self.validate_login(response)
         # get url of timetable
         # TODO: this xpath expression is ugly...It's probably better to rewrite it someday.
+        self.log(level=logging.INFO, message='Login succeed')
         timetable_url = response.xpath("//div[@class='nav-collapse']/ul/li[2]/a/@href").get()
-        if timetable_url is not None:
-           # yield scrapy.Request(timetable_url, callback=self.parse_timetable)
-            yield response.follow(timetable_url, callback=self.parse_timetable)
-        else:
-            raise ValueError("Invalid timetable url {}".format(timetable_url))
+        yield response.follow(timetable_url, callback=self.parse_timetable)
 
     def parse_timetable(self, response):
         # TODO: Doc required
@@ -90,8 +90,6 @@ class TimeTableSpider(scrapy.Spider):
             # Add subject_name to class_info
             class_info['subject_name'] = subject_pair[class_info['subject_code']]
             yield class_info
-
-        self.log("{} class(s) information retrieved".format(len(timetable)))
 
     def match_code_name(self, response):
         # TODO: Documentation required
@@ -134,7 +132,7 @@ class ClassContainerIterator:
     def __next__(self):
         # TODO: Doc required
         # Stop iteration if last element in classContainer has been reached
-        if self.num > len(self.classContainers):
+        if self.num >= len(self.classContainers):
             raise StopIteration
         else:
             class_info = Class()
