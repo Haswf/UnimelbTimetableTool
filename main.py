ea305e7f27a3f97e53f2df99352a10728b682995
Haswell Fan <haswell.fan@outlook.com>
2019-08-08 15:55:37 +1000

    Fix main.py

--- main.py
+++ main.py
@@ -3,21 +3,8 @@ from gcalendar import calendar_api
 
 # import timetablespider from spider class
 from timetable.spiders.timetable import TimeTableSpider
-
-from scrapy import signals
-from scrapy.crawler import CrawlerRunner
-from twisted.internet import reactor
-from scrapy.crawler import Crawler
-from scrapy.settings import Settings
-import logging
-
+from timetable import crawl
 
 if __name__ == "__main__":
-    crawler = Crawler(TimeTableSpider)
-    #runner = CrawlerRunner()
-    #runner.crawl(TimeTableSpider)
-    crawler.crawl()
-    #calendar_api.create_timetable()
-
-
+    crawl.run()
 
